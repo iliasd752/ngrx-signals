d0c43abdc0e18c532cfe0863181d9d108f0e17ef
Ilias <ilias.dinia@outlook.com>
2024-03-03 18:20:13 +0200

    Implemented the AddTodo functionality

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {TODOS} from "../model/mock-data";
+import {Todo} from "../model/todo.model";
 
 
 @Injectable({
@@ -14,6 +15,14 @@ export class TodosService {
     return TODOS;
   }
 
+  async addTodo(todo: Partial<Todo>): Promise<Todo> {
+
+    await sleep(1000);
+    return {
+      id: Math.random().toString(36).substr(2,9),
+      ...todo
+    } as Todo;
+  }
 
 }
 
